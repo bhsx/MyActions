ec0038c50adb88f6d0228981874867773ca51c83
bhsx <gk747@163.com>
2022-05-15 21:26:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ console.log("hi node.js");
 process.argv.forEach((val, index) => {
   console.log(`${"_"+index+"_"}: ${"_"+val+"_"}`)
 })
-
+let a=JSON.stringify(process.argv)
+console.log(a)
 // console.log(JSON.parse(process.argv.TEST1))
