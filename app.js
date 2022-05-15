a13d915a78167c4a4ecf889b18aa2860a581e0e5
bhsx <gk747@163.com>
2022-05-15 21:49:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ console.log("hi node.js");
 process.argv.forEach((val, index) => {
   console.log(`${"_"+index+"_"}: ${"_"+val+"_"}`)
 })
-let b=process.argv[2].split("")
-console.log(b.join())
+// let b=process.argv[2].split("")
+console.log(process.argv[2].substr(0,10))
 // let a=JSON.parse(process.argv[2])
 // console.log(JSON.parse(process.argv.TEST1))
 
