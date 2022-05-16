747095091c1dd5df1a2eeb95d953428678d5a8dc
GK <gk747@163.com>
2022-05-16 11:30:16 +0800

    1

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 console.log("hi node.js");
 process.argv.forEach((val, index) => {
-  console.log(`${"_"+index+"_"}: ${"_"+val+"_"}`)
+    console.log(`${"_" + index + "_"}: ${"_" + val + "_"}`)
 })
-// let b=process.argv[2]
-console.log(b.substr(0,b.lenght-2)
+let b = process.argv[2]
+console.log(b.substring(0, b.length - 2))
 // let a=JSON.parse(process.argv[2])
 // console.log(JSON.parse(process.argv.TEST1))
 
