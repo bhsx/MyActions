1c3f6e7e3dd7e8aedc3a37e6db93ca8334410f64
GK <gk747@163.com>
2022-05-16 11:31:31 +0800

    1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ process.argv.forEach((val, index) => {
     console.log(`${"_" + index + "_"}: ${"_" + val + "_"}`)
 })
 let b = process.argv[2]
-console.log(b.substring(0, b.length - 2))
+console.log(b.substring(0, b.length - 1))
 // let a=JSON.parse(process.argv[2])
 // console.log(JSON.parse(process.argv.TEST1))
 
